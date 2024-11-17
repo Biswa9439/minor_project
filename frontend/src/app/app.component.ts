ca98471eda6076f81bceb3b7dc2d88594551b5b8
prakashnaxtre123 <prakash.naxtre@gmail.com>
2024-11-17 23:06:31 +0530

    commit

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,38 +1,10 @@
-import { AfterViewInit, Component, ElementRef, OnChanges, OnInit, QueryList, SimpleChanges, ViewChild, ViewChildren } from '@angular/core';
-import { ChildComponent } from './child/child.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit,AfterViewInit {
-  @ViewChildren('highlight') marker!: QueryList<any>;
-  @ViewChild('child') child!: ChildComponent;
-  constructor(){
-
-  }
-  ngOnInit(): void {
-    console.log(this.marker)
-  }
-  ngAfterViewInit(): void {
-    console.log(this.marker)
-    console.log(this.child.counter)
-    //this.marker.nativeElement.style.color = "red"
-    this.marker.map((item: any) => {
-      item.nativeElement.style.color = "red"
-    })
-  }
-  inc(){
-    this.child.increment()
-    console.log(this.child.counter)
-  }
-  dec(){
-    this.child.decrement()
-    console.log(this.child.counter)
-  }
-  reset(){
-    this.child.reset()
-    console.log(this.child.counter)
-  }
+export class AppComponent {
+  title = 'Angular 16 Crud example';
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,30 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChildComponent } from './child/child.component';
-import { PropertyBindingComponent } from './property-binding/property-binding.component';
-import { TableViewComponent } from './table-view/table-view.component';
-import { AgePipe } from './age.pipe';
-import { TitlePipe } from './title.pipe';
-import { EmpCountComponent } from './emp-count/emp-count.component';
-import { FormsModule } from '@angular/forms';
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
+import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildComponent,
-    PropertyBindingComponent,
-    TableViewComponent,
-    AgePipe,
-    TitlePipe,
-    EmpCountComponent
+    AddTutorialComponent,
+    TutorialDetailsComponent,
+    TutorialsListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
